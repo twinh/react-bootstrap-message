7b23f77eb0fc30dca7e3247bc66108d144468945
twinh <twinhuang@qq.com>
2018-11-28 09:12:23 +0800

    feat: 更改为antd的message

--- js/react-bootstrap-message.js
+++ js/react-bootstrap-message.js
@@ -1,38 +1,90 @@
+// From https://github.com/ant-design/ant-design/blob/master/components/message/index.tsx
+// 1. Removes icon dependence
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Notification from 'rc-notification';
 
-const Message = (props) => {
-  return <div className={"message-content message-content-" + props.type}>{props.children}</div>;
-};
+let defaultTop = 0;
+let messageInstance = 0;
+let key = 1;
+let prefixCls = 'message';
+let transitionName = 'message-pop';
+let getContainer = 0;
+let maxCount = 0;
 
-Message.defaultProps = {
-  type: 'success',
-};
-
-let notification = null;
-Notification.newInstance({
-  prefixCls: 'message',
-  animation: 'pop',
-  style: {}, // 移除默认的样式
-}, (n) => notification = n);
-
-Message.message = (content, duration = 2, type, onClose) => {
-  const key = Date.now();
-  notification.notice({
-    content: <Message type={type}>{content}</Message>,
-    duration: duration,
-    prefixCls: 'message',
-    style: {}, // 移除默认的样式
-    key,
-    onClick: () => {
-      notification.removeNotice(key);
+function getMessageInstance(callback) {
+  if (messageInstance) {
+    callback(messageInstance);
+    return;
+  }
+  Notification.newInstance({
+    prefixCls: prefixCls,
+    transitionName: transitionName,
+    style: {top: defaultTop},
+    getContainer: getContainer,
+    maxCount: maxCount
+  }, function (instance) {
+    if (messageInstance) {
+      callback(messageInstance);
+      return;
     }
+    messageInstance = instance;
+    callback(instance);
   });
+}
+
+function notice(args) {
+  const duration = args.duration !== undefined ? args.duration : defaultDuration;
+  const target = key++;
+
+  const closePromise = new Promise(function (resolve) {
+    const callback = () => {
+      if (typeof args.onClose === 'function') {
+        args.onClose();
+      }
+      return resolve(true);
+    };
+    getMessageInstance(function (instance) {
+      instance.notice({
+        key: target,
+        duration: duration,
+        style: {}, // 移除默认的样式
+        content: <div className={"message-content message-content-" + args.type}>{args.content}</div>,
+        onClose: callback,
+      });
+    });
+  });
+  const result = function result() {
+    if (messageInstance) {
+      messageInstance.removeNotice(target);
+    }
+  };
+  result.then = function (filled, rejected) {
+    return closePromise.then(filled, rejected);
+  };
+  result.promise = closePromise;
+  return result;
+};
+
+const api = {
+  open: notice,
+  destroy: function destroy() {
+    if (messageInstance) {
+      messageInstance.destroy();
+      messageInstance = null;
+    }
+  }
 };
 
 ['success', 'danger', 'warning', 'info', 'dark'].forEach((type) => {
-  Message[type] = (content, duration, onClose) => Message.message(content, duration, type, onClose);
+  api[type] = function (content, duration, onClose) {
+    if (typeof duration === 'function') {
+      onClose = duration;
+      duration = undefined;
+    }
+    return api.open({content: content, duration: duration, type: type, onClose: onClose});
+  };
 });
 
-export default Message;
+export default api;
